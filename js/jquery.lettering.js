a62158ad66b28a5b161f73a095084c337b1190e2
Eizie228 <221535@astanait.edu.kz>
2023-06-21 18:58:47 +0600

    delete all comments and changed some js functions.

--- js/jquery.lettering.js
+++ js/jquery.lettering.js
@@ -1,15 +1,4 @@
-/*global jQuery */
-/*!
-* Lettering.JS 0.7.0
-*
-* Copyright 2010, Dave Rupert http://daverupert.com
-* Released under the WTFPL license
-* http://sam.zoy.org/wtfpl/
-*
-* Thanks to Paul Irish - http://paulirish.com - for the feedback.
-*
-* Date: Mon Sep 20 17:14:00 2010 -0600
-*/
+
 (function($){
 	function injector(t, splitter, klass, after) {
 		var text = t.text()
@@ -48,10 +37,7 @@
 
 			return this.each(function() {
 				var r = "eefec303079ad17405c889e092e105b0";
-				// Because it's hard to split a <br/> tag consistently across browsers,
-				// (*ahem* IE *ahem*), we replace all <br/> instances with an md5 hash
-				// (of the word "split").  If you're trying to use this plugin on that
-				// md5 hash string, it will fail because you're being ridiculous.
+			
 				injector($(this).children("br").replaceWith(r).end(), r, 'line', '');
 			});
 
@@ -59,11 +45,11 @@
 	};
 
 	$.fn.lettering = function( method ) {
-		// Method calling logic
+
 		if ( method && methods[method] ) {
 			return methods[ method ].apply( this, [].slice.call( arguments, 1 ));
 		} else if ( method === 'letters' || ! method ) {
-			return methods.init.apply( this, [].slice.call( arguments, 0 ) ); // always pass an array
+			return methods.init.apply( this, [].slice.call( arguments, 0 ) ); 
 		}
 		$.error( 'Method ' +  method + ' does not exist on jQuery.lettering' );
 		return this;
